2b32ca1a53af02f685ef5bcc6a2bc5ea620abfea
IST-PC-0072 <IST-PC-0072@ad.hsinni.com>
2022-07-27 07:32:09 +0700

    'rani'

--- application/views/kaizen_submit.php
+++ application/views/kaizen_submit.php
@@ -1,5 +1,15 @@
 <!-- <script src="https://code.highcharts.com/highcharts.js"></script> -->
+<style>
+    span.a {
+  display: inline; /* the default for span */
+  /* width: 100px;
+  height: 100px;
+  padding: 5px; */
+  /* border: 1px solid blue;   */
+  background-color: yellow; 
+}
 
+    </style>
 <script src="<?php echo base_url();?>template/highcharts/highcharts.js"></script>
 <script src="<?php echo base_url();?>template/highcharts/exporting.js"></script>
 <script src="<?php echo base_url();?>template/highcharts/export-data.js"></script>
@@ -9,8 +19,11 @@
 <script src="https://code.highcharts.com/modules/exporting.js"></script>
 <script src="https://code.highcharts.com/modules/export-data.js"></script>
 <script src="https://code.highcharts.com/modules/accessibility.js"></script> -->
-<div class="div-1" style=" height=100%;">
-<h1><span  style="background-color: rgba(220,0,0,1)"><center>TARGET KAIZEN 2022</center></span></h1>
+<div class="div-1" style=" height=100%; text-align:center">
+<br>
+<span style=" padding : 10px;background-color:#F499CF; font-size:20pt; font-weight:bold">Target Kaizen 2022</span>
+    <br>
+    <br>
     <br>
 <div class="row">
     
@@ -22,20 +35,7 @@
         
         </p>
     </figure>
-    <table class="table table-sm table-bordered">
-        <tr style="background-color:#FF9633">
-            <th>Est. Direct Labor</th>
-            <th>Target(1.0)</th>
-            <th>Actual</th>
-            <th>Ratio</th>
-        </tr>
-        <tr style="background-color:#EEEAE7">
-            <th>13000</th>
-            <th>13000</th>
-            <th>1351</th>
-            <th><?php echo date('m');?></th>
-        </tr>
-    </table>
+    
 </div>
 <div class="col-md-6">
     <h3 style="color:White;"><center>Kaizen Impact to Efficiency</center></h3>
@@ -45,48 +45,36 @@
         
         </p>
     </figure>
-    <table class="table table-sm table-bordered">
-        <tr style="background-color:#FF9633">
-            <th>Est. Direct Labor</th>
-            <th>Target(1.0)</th>
-            <th>Actual</th>
-            <th>Ratio</th>
-        </tr>
-        <tr style="background-color:#EEEAE7">
-            <th>13000</th>
-            <th>13000</th>
-            <th>1351</th>
-            <th><?php echo date('m');?></th>
-        </tr>
-    </table>
+    
 </div>
 </div>
 </div>
 <script>
 
 
-Highcharts.wrap(Highcharts.Series.prototype, 'drawLegendSymbol', function (proceed, legend) {
-    proceed.call(this, legend);
+// Highcharts.wrap(Highcharts.Series.prototype, 'drawLegendSymbol', function (proceed, legend) {
+//     proceed.call(this, legend);
 
-    this.legendLine.attr({
-        d: ['M', 0, 10, 'L', 5, 5, 8, 10]
-    });
-    this.negativeLine = this.chart.renderer.path(
-        ['M', 8, 10, 'L', 11, 15, 16, 10]
-    ).attr({
-        stroke: this.options.negativeColor,
-        'stroke-width': this.options.lineWidth
-    })
-        .add(this.legendGroup);
-});
+//     this.legendLine.attr({
+//         d: ['M', 0, 10, 'L', 5, 5, 8, 10]
+//     });
+//     this.negativeLine = this.chart.renderer.path(
+//         ['M', 8, 10, 'L', 11, 15, 16, 10]
+//     ).attr({
+//         stroke: this.options.negativeColor,
+//         'stroke-width': this.options.lineWidth
+//     })
+//         .add(this.legendGroup);
+// });
 
 
 Highcharts.chart('container', {
     chart: {
         backgroundColor: '#1B5EA9',
         zoomType: 'xy',
-        height: (9 / 16 * 100) + '%' // 16:9 ratio
+        // height: (9 / 16 * 100) + '%' // 16:9 ratio
     },
+    
     title: {
         text: null,
         enabled : false
@@ -172,16 +160,22 @@ Highcharts.chart('container', {
     tooltip: {
         shared: true
     },
-    legend: {
-        layout: 'vertical',
-        align: 'left',
-        x: 120,
-        verticalAlign: 'top',
-        y: 100,
-        floating: true,
-        backgroundColor:
-            Highcharts.defaultOptions.legend.backgroundColor || // theme
-            'rgba(255,255,255,0.25)'
+    // legend: {
+    //     // layout: 'vertical',
+    //     // align: 'center',
+    //     // x: 0,
+    //     // verticalAlign: 'bottom',
+    //     // y: 0,
+    //     floating: true,
+    //     backgroundColor:
+    //         Highcharts.defaultOptions.legend.backgroundColor || // theme
+    //         'rgba(255,255,255,0.25)',
+           
+    // },
+    legend:{
+        itemStyle: {
+            color: 'white'
+        }
     },
     series: [{
         name: 'Actual',
@@ -199,10 +193,10 @@ Highcharts.chart('container', {
                 }
 
                 if ($i < count($implemented) - 1 ){
-                    $jam = "[".number_format((float)$implemented[$i]['RATIO'], 2, '.', '')."],";
+                    $jam = "[".number_format((float)$implemented[$i]['ACTUAL_IDE'], 2, '.', '')."],";
                 }
                 else{
-                    $jam ="[".number_format((float)$implemented[$i]['RATIO'], 2, '.', '')."]";
+                    $jam ="[".number_format((float)$implemented[$i]['ACTUAL_IDE'], 2, '.', '')."]";
                 }
                 echo $jam;
             
@@ -211,12 +205,21 @@ Highcharts.chart('container', {
 
         ?>
         label:{
-            visible : false,
+            visible : true,
         },
         tooltip: {
             valueSuffix: ''
         },
-        color: '#F51C1C'
+        color: '#F51C1C',
+        dataLabels: {
+                enabled: true,
+                shape : 'square',
+                backgroundColor: '#FFFFFF',
+                style: {
+                    color: '#00000',
+                    textOutline: 'none'
+                }
+            }
 
        
     }, {
@@ -225,7 +228,7 @@ Highcharts.chart('container', {
         <?php 
             echo "data: [";
             for ($i=0; $i<count($implemented); $i++){
-                $data = number_format((float)$implemented[$i]['RATIO'], 2, '.', '');
+                $data = number_format((float)$implemented[$i]['JML_KARYAWAN'], 2, '.', '');
             if ($i < count($implemented) - 1 ){
                 $jam = "[".$data."],";
                 }
@@ -246,170 +249,118 @@ Highcharts.chart('container', {
         },
         dataLabels: {
                 enabled: true,
-                
+                shape : 'square',
+                backgroundColor: '#F7DE4E',
+                style: {
+                    color: '#00000',
+                    textOutline: 'none'
+                },
+                // enabled: true,
+                borderRadius: 5,
+                // backgroundColor: 'rgba(252, 255, 197, 0.7)',
+                borderWidth: 1,
+                borderColor: '#AAA',
+                y: -10
             },
-        plotOptions: {
-        series: {
-            label: {
-                connectorAllowed: false
-            },
-            pointStart: 2010
-        }
-    },
+        // plotOptions: {
+        // series: {
+        //     label: {
+        //         connectorAllowed: false
+        //     },
+        //     pointStart: 2010
+        // }
+    // },
     }]
 });
 
-
-// Highcharts.chart('containers', {
-//     chart: {
-//         backgroundColor: '#1B5EA9',
-//         zoomType: 'xy',
-//         height: (9 / 16 * 100) + '%' // 16:9 ratio
-
-//     },
-//     title: {
-//         text: null//'Kaizen Implemented'
-//     },
-//    credits:{
-//         enabled: false
-//    },
-
-//     xAxis: [{
-//         <?php 
-//             echo "categories: [";
-//             for ($i=0; $i<count($implemented); $i++){
-               
-//                 if ($i < count($implemented) - 1 ){
-//                      $bulan = "['".$implemented[$i]['BULAN']."'],";
-//                 }
-//                 else{
-//                     $bulan ="['".$implemented[$i]['BULAN']."']";
-//                 }
-//                 echo $bulan;
-//             // echo "{ y: ".$hasilData->QTY_SEKARANG.", color:'".$warna."'},";
-//         }
-//         echo "],";
-//         ?>
-//         crosshair: true
-//     }],
-//     yAxis: [{ // Primary yAxis
-//         labels: {
-//             // format: '{value}°C',
-//             style: {
-//                 color: Highcharts.getOptions().colors[1]
-//             }
-//         },
-//         title: {
-//             // text: 'Target',
-//             style: {
-//                 color: Highcharts.getOptions().colors[3]
-//             },
-//             enabled : false
-//         },
-//         series: {
-//             color: '#FF0000'
-//         }
-//     },
-//     { // Secondary yAxis
-//         min : 0,
-//         max : 1,
-//         title: {
+Highcharts.chart('containers', {
+    chart: {
+        type: 'column',
+        backgroundColor: '#1B5EA9'
+    },
+    title: {
+        text: null
+    },
+    credits:{
+        enabled: false
+    },
+    subtitle: {
+        text: null
+    },
+    legend:{
+        itemStyle: {
+            color: 'white'
+        }
+    },
+    xAxis: {
+        categories: [
+            'Jan',
+            'Feb',
+            'Mar',
+            'Apr',
+            'May',
+            'Jun',
+            'Jul',
+            'Aug',
+            'Sep',
+            'Oct',
+            'Nov',
+            'Dec'
+        ],
+        crosshair: true,
+        labels: {
+            style: {
+                color: 'white'
+            }
+        }
+    },
+    yAxis: {
+        min: 0,
+        title: {
+            text: 'Rainfall (mm)'
+        },
+        labels: {
+            style: {
+                color: 'white'
+            }
+        }
+    },
+    tooltip: {
+        headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+        pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+            '<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
+        footerFormat: '</table>',
+        shared: true,
+        useHTML: true
+    },
+    plotOptions: {
+        column: {
+            pointPadding: 0.2,
+            borderWidth: 0
+        }
+    },
+    series: [{
+        name: 'Tokyo',
+        data: [49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4],
+        color: '#F7DE4E',
+        dataLabels: {
+                enabled: true,
+                shape : 'square',
+                backgroundColor: '#FFFFFF',
+                style: {
+                    color: '#00000',
+                    textOutline: 'none'
+                },
+                borderRadius: 5,
+                borderWidth: 1,
+                borderColor: '#AAA',
+                y: -10
+            },
            
-//             text: 'Actual',
-//             style: {
-//                 color: Highcharts.getOptions().colors[2]
-//             },
-//             enabled : false
-//         },
-//         labels: {
-//             // format: '{value} mm',
-//             style: {
-//             color: Highcharts.getOptions().colors[2]
-//             },
-//             enabled : false
-//         },
-//         opposite: true
-//     }],
-//     tooltip: {
-//         shared: true
-//     },
-//     legend: {
-//         layout: 'vertical',
-//         align: 'left',
-//         x: 120,
-//         verticalAlign: 'top',
-//         y: 100,
-//         floating: true,
-//         backgroundColor:
-//             Highcharts.defaultOptions.legend.backgroundColor || // theme
-//             'rgba(255,255,255,0.25)'
-//     },
-//     series: [{
-//         name: 'Actual',
-//         type: 'column',
-//         yAxis: 1,
-//         <?php 
-//             echo "data: [";
-//             for ($i=0; $i<count($implemented); $i++){
-//                 $bulan = date('m');
-//                 if ($bulan == $implemented[$i]['PERIODE'] )
-//                 {
-//                     $warna = '#FF9633'; //oranye
-//                 }else{
-//                     $warna = '#EEEAE7'; //abuabu
-//                 }
-
-//                 if ($i < count($implemented) - 1 ){
-//                     $jam = "{y:".number_format((float)$implemented[$i]['RATIO'], 2, '.', '').", color: '".$warna."'},";
-//                 }
-//                 else{
-//                     $jam ="{y:".number_format((float)$implemented[$i]['RATIO'], 2, '.', '').", color: '".$warna."'}";
-//                 }
-//                 echo $jam;
-            
-//             }
-//             echo "],";
-
-//         ?>
-//         color: '#FF9633',
-//         label:{
-//             visible : false
-//         },
-//         tooltip: {
-//             valueSuffix: ''
-//         },
-
-       
-//     }, {
-//         name: 'Target',
-//         type: 'spline',
-//         <?php 
-//             echo "data: [";
-//             for ($i=0; $i<count($implemented); $i++){
-//                 $data = number_format((float)$implemented[$i]['RATIO'], 2, '.', '');
-//             if ($i < count($implemented) - 1 ){
-//                 $jam = "[".$data."],";
-//                 }
-//                 else{
-//                 $jam ="[".$data."]";
-//                 }
-//             echo $jam;
-//             // echo "{ y: ".$hasilData->QTY_SEKARANG.", color:'".$warna."'},";
-//         }
-//         echo "],";
-//         ?>
-//         // style: {
-//         //         color: Highcharts.getOptions().colors[3]
-//         //     },
-//         color: '#FF9633',
-//         tooltip: {
-//             valueSuffix: ''
-//         },
-//         dataLabels: {
-//                 enabled: true
-//             }
-//     }]
-// });
+    },
+    
+    ]
+});
               
 </script>
 
